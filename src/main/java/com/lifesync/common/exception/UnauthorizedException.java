ae2956ada0f57f56bb384dbfaff055860bf13218
hari antarkar <harishantarkar3738@gmail.com>
2026-08-18 13:59:04 +0530

    chore: remove stale duplicate backend files from repo root

--- src/main/java/com/lifesync/common/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.lifesync.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class UnauthorizedException extends ApiException {
-    public UnauthorizedException(String message) {
-        super(message, HttpStatus.UNAUTHORIZED);
-    }
-}
--- src/main/java/com/lifesync/user/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.lifesync.user.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
